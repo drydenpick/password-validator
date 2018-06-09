a6b42caf31e3ec7cfd831cdc9c280f3aeb54fe4a
Dryden Pick <dr286855@dal.ca>
2018-06-09 00:09:35 -0300

    Completing stage2!

--- app/src/main/java/com/assignment/dryden/assignmentvalidator/Validator.java
+++ app/src/main/java/com/assignment/dryden/assignmentvalidator/Validator.java
@@ -6,6 +6,15 @@ public class Validator {
             result++;
         if(!password.toLowerCase().equals("password"))
             result++;
+        if(!password.toLowerCase().equals(password))
+            result++;
+        if(!password.toUpperCase().equals(password))
+            result++;
+        if(password.contains("0") || password.contains("1") || password.contains("2")
+                || password.contains("3") || password.contains("4") || password.contains("5")
+                || password.contains("6") || password.contains("7") ||  password.contains("8")
+                || password.contains("9"))
+            result++;
         return result;
     }
 }
--- app/src/test/java/com/assignment/dryden/assignmentvalidator/ValidatorUnitTest.java
+++ app/src/test/java/com/assignment/dryden/assignmentvalidator/ValidatorUnitTest.java
@@ -9,9 +9,11 @@ import static org.junit.Assert.assertEquals;
 public class ValidatorUnitTest {
     @Test
     public void validatorTester() throws Exception {
-        assertEquals(Validator.validate("password"), 1);
-        assertEquals(Validator.validate("password1"), 2);
-        assertEquals(Validator.validate("passwo"), 1);
-    }
+        assertEquals(Validator.validate("password"), 2);
+        assertEquals(Validator.validate("Password123"),5);
+        assertEquals(Validator.validate("password123"), 4);
+        assertEquals(Validator.validate("PASSWORD123"),4);
+        assertEquals(Validator.validate("Pa5"),4);
+        assertEquals(Validator.validate(""), 1);
     }
 }
